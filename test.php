d0d577e24f20984a7921f66142e40c4e4acfb516
Julián Rosalen <rosalenjulian@gmail.com>
2021-09-10 16:55:29 -0300

    first hml code

---
+++ test.php
@@ -0,0 +1,23 @@
+
+<head>
+	<title>Calculator</title>
+</head>
+
+<body>
+    <div id="page-wrap">
+	<h1>Calculator</h1>
+	  <form action="" method="post" id="quiz-form">
+            <p>
+                <input type="number" name="first_num" id="first_num" required="required" value="" /> <b>First Number</b>
+            </p>
+            <p>
+                <input type="number" name="second_num" id="second_num" required="required" value="" /> <b>Second Number</b>
+            </p>
+            <input type="submit" name="operator" value="+" />
+            <input type="submit" name="operator" value="-" />
+            <input type="submit" name="operator" value="*" />
+            <input type="submit" name="operator" value="/" />
+        </form>
+    </div>
+</body>
+</html>

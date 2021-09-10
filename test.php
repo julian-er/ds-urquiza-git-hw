802dd3e09564705a6f46fb3a5e5654fe49c513f6
Julián Rosalen <rosalenjulian@gmail.com>
2021-09-10 16:55:59 -0300

    PHP code

--- test.php
+++ test.php
@@ -3,21 +3,47 @@
 	<title>Calculator</title>
 </head>
 
+<?php
+$first_num = $_POST['first_num'];
+$second_num = $_POST['second_num'];
+$operator = $_POST['operator'];
+$result = '';
+if (is_numeric($first_num) && is_numeric($second_num)) {
+    switch ($operator) {
+        case "+":
+           $result = $first_num + $second_num;
+            break;
+        case "-":
+           $result = $first_num - $second_num;
+            break;
+        case "*":
+            $result = $first_num * $second_num;
+            break;
+        case "/":
+            $result = $first_num / $second_num;
+    }
+}
+
+?>
+
 <body>
     <div id="page-wrap">
 	<h1>Calculator</h1>
 	  <form action="" method="post" id="quiz-form">
             <p>
-                <input type="number" name="first_num" id="first_num" required="required" value="" /> <b>First Number</b>
+                <input type="number" name="first_num" id="first_num" required="required" value="<?php echo $first_num; ?>" /> <b>First Number</b>
             </p>
             <p>
-                <input type="number" name="second_num" id="second_num" required="required" value="" /> <b>Second Number</b>
+                <input type="number" name="second_num" id="second_num" required="required" value="<?php echo $second_num; ?>" /> <b>Second Number</b>
             </p>
             <input type="submit" name="operator" value="+" />
             <input type="submit" name="operator" value="-" />
             <input type="submit" name="operator" value="*" />
             <input type="submit" name="operator" value="/" />
         </form>
+        <p>
+            <?php echo $result; ?>
+        </p>
     </div>
 </body>
 </html>
